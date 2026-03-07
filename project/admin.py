d49772396f4ac60974d5f3df120ae02d097b73a3
aseel rostom <asilrostom15612@gamil.com>
2026-03-07 23:34:36 +0200

    first commit

---
+++ project/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import college, customer,product,order
+
+admin.site.register(college)
+admin.site.register(customer)
+admin.site.register(product)
+admin.site.register(order)
+
