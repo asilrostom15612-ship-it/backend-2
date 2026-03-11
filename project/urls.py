619eb91da197cda6ae735be3d1320b896ab2538c
aseel rostom <asilrostom15612@gamil.com>
2026-03-11 21:19:29 +0200

    Initial commit

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   
-  # path('login/', views.login, name='login'),
-  # path('logout', views.logout, name='logout'),
+  path('signin/', views.signin_user, name='signin'),
+  path('signup/', views.signup_user, name='signup'),
+  path('logout/', views.logout_user, name='logout'),
+  
 ]
--- project/views.py
+++ project/views.py
@@ -1,4 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib.auth.models import User
+from django.contrib import messages 
+
 
 def home(request):
     return render (request, 'home.html',{})
+
+def signin_user (request):
+    if request.method == "POST":
+       username=request.POST['username']
+       password=request.POST['password']
+       user =authenticate(request,username=username, password=password)
+       if user is not None:
+        login(request,user)
+        messages.success(request,("You have been logged in.")) 
+        return redirect('home')
+       else:
+          messages.error(request,("There was an error ,please try again."))
+          return redirect ('Sign-in')
+    else:
+     return render (request, 'Sign-in.html',{})
+
+
+def logout_user (request):
+    logout(request)
+    messages.success(request, ("You hane been logged out."))
+    return redirect('home')
+
+
+def signup_user(request):
+    if request.method == "POST":
+       
+       username=request.POST['username']
+       password=request.POST['password']
+       if User.objects.filter(username=username).exists():
+          messages.error(request, ("Username already exists."))
+          return redirect ('signin')
+       else:
+          user =User.objects.create_user(username=username, password=password)
+          user.save()
+          login(request,user)
+          messages.success(request,("Account created successfully.")) 
+          return redirect('home')
+    else:
+      return render(request,'Sign-in.html', {})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
